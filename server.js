2ecc9d4b16130c738a66bd7768ca6d768549c8e8
Letícia Zalasik <leticiazalasik@gmail.com>
2025-01-09 13:44:46 -0300

    :sparkles: Feat: Atualizando o projeto para manter as mensagens no chat.

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');  // Importando o pacote CORS
 
 const app = express();
-const server = require ('http').createServer(app);
-const io = require('socket.io')(server);
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname,'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-app.use('/', (req,res)=> {
-    res.render('index.html');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'http://127.0.0.1:5500',  // Permite somente o front-end a partir de http://127.0.0.1:5500
+        methods: ['GET', 'POST'],  // Permite métodos GET e POST
+        allowedHeaders: ['my-custom-header'],  // Caso precise de headers personalizados
+        credentials: true  // Permite o envio de cookies (se necessário)
+    }
 });
+
+app.use(cors());  // O CORS também é usado para outras requisições HTTP
+
+// Serve arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configuração para servir o arquivo index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+let messages = [];
 
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`);
-  socket.emit('welcome', 'Bem-vindo ao servidor!');
+
+    socket.emit('previousMessages', messages);
+  
+    socket.on('sendMessage', data => {
+        messages.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+    });
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
